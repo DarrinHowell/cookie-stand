349a27455421737c05656b509f222f51fbf6b590
Darrin Howell <dbhowell09@gmail.com>
2018-09-17 15:38:07 -0700

    Initialized market object with startup data

---
+++ js/app.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var market = {
+  locations: [
+    {address: '1st and Pike',
+      minCust: 23,
+      maxCust: 65,
+      avgCookies: 6.3
+    },
+
+    {address: 'SeaTac Airport',
+      minCust: 3,
+      maxCust: 24,
+      avgCookies: 1.2
+    },
+
+    {address: 'Seattle Center',
+      minCust: 24,
+      maxCust: 38,
+      avgCookies: 3.7
+    },
+
+    {address: 'Capitol Hill',
+      minCust: 20,
+      maxCust: 38,
+      avgCookies: 2.3
+    },
+
+    {
+      address: 'Alki',
+      minCust: 2,
+      maxCust: 16,
+      avgCookies: 4.6
+    }
+
+  ],
+
+  // function randNumCookies() {
+  // console.log("Runs function");
+  // }
+
+};
+
