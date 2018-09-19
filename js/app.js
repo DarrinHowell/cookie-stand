2f885fcde0be65f4a438e2b20413bfb39f3c49e0
Darrin Howell <dbhowell09@gmail.com>
2018-09-19 00:15:39 -0700

    completed rendering of table and fixed issue with totaling cookies for each location; still need to style table

--- js/app.js
+++ js/app.js
@@ -48,13 +48,15 @@ Location.prototype.randNumCustGenerator = function() {
 
 // build out the cookiesPurchased arr using random number generator
 Location.prototype.cookiesPurchasedAtEachHour = function() {
-  for(var i = 0; i < tableHeadLabels.length; i++){
+  for(var i = 0; i < tableHeadLabels.length-1; i++){
     var numCust = this.randNumCustGenerator();
     //console.log(numCust);
     var cookiesPurchasedNow = Math.floor(numCust * this.avgCookies);
     this.cookiesPurchased.push(cookiesPurchasedNow);
   }
+  this.cookiesPurchased.push(this.appendTotalCookies());
 };
+
 
 // create a new message that concatonates values from different arrays
 // this text will be displayed in our webpage.
@@ -65,15 +67,17 @@ Location.prototype.createResultsList = function() {
   }
 };
 
-Location.prototype.appendTotalCookAndLabels = function() {
+
+Location.prototype.appendTotalCookies = function() {
   var sum = 0;
-  for(var i = 0; i < tableHeadLabels.length; i++){
+  for(var i = 0; i < tableHeadLabels.length-1; i++){
     sum += this.cookiesPurchased[i];
   }
   // var message = 'Total: ' + sum + ' cookies';
   //this.resultsList.push(message);
-  this.resultsList.push(sum);
+  return sum;
 };
+
 
 // render list in html
 Location.prototype.renderResultsRow = function() {
@@ -83,7 +87,7 @@ Location.prototype.renderResultsRow = function() {
   // generate a new row each time we run computations on a new object
   var newRow = document.createElement('tr');
 
-  // create a row showing projection of num cookies purchased at each hour for each location 
+  // create a row showing projection of num cookies purchased at each hour for each location
   for(var i = 0; i < this.cookiesPurchased.length; i++){
 
     if(i === 0){
@@ -104,6 +108,7 @@ Location.prototype.renderResultsRow = function() {
 
 };
 
+
 Location.locations = [];
 
 ////////////////////////////////////////////////////////////////////////////////////////
@@ -117,7 +122,6 @@ var seaTac = new Location('SeaTac Airport', 3, 24, 1.2);
 var seattleCenter = new Location('Seattle Center', 11, 38, 3.7);
 var capitolHill = new Location('Capitol Hill', 20, 38, 2.3);
 var alki = new Location('Alki', 2, 16, 4.6);
-console.log(Location.locations);
 
 ////////////////////////////////////////////////////////////////////////////////////////
 
@@ -156,20 +160,41 @@ function tableHeadGenerator(tableHeadLabels){
 
 }
 
+function tableFooterGenerator(tableHeadLabels){
+  var position = document.getElementsByTagName('table')[0];
+  var newRow = document.createElement('tr');
+  for(var i = 0; i < tableHeadLabels.length; i++){
+    if(i === 0){
+      var newTd = document.createElement('td');
+      var newTdText = document.createTextNode('Total');
+      newTd.appendChild(newTdText);
+    } else {
+      newTd = document.createElement('td');
+      newTdText = document.createTextNode('');
+      newTd.appendChild(newTdText);
+    }
+    newRow.appendChild(newTd);
+  }
+  position.appendChild(newRow);
+}
+
 
 ////////////////////////////////////////////////////////////////////////////////////////
 
 // generate table head + labels
 tableHeadGenerator(tableHeadLabels);
 
-// run methods, generate sales data,
+// run methods, generate sales data, render table rows one by one
 for(var i = 0; i < Location.locations.length; i++){
   var store = Location.locations[i];
   store.cookiesPurchasedAtEachHour();
   store.createResultsList();
-  store.appendTotalCookAndLabels();
+  store.appendTotalCookies();
   store.renderResultsRow();
   // console.log(store.cookiesPurchased);
   // console.log(store.resultsList);
   //store.renderResultsList();
 }
+
+// generate last row of table
+tableFooterGenerator(tableHeadLabels);
